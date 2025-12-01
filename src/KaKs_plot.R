1ec585cb07688b18c257cc034d44e441e15ce0f1
Raul Duran De Alba <rda2799@gmail.com>
2025-12-01 03:05:45 +0100

    Uploaded initial data on KaKs from only 8.88% of initial pairs of genes. Need to copy correctly data from Mikes computer. Added plotting R script for KaKs values distribution.

---
+++ src/KaKs_plot.R
@@ -0,0 +1,25 @@
+library(ggplot2)
+
+kaks <- read.table("/Users/raulduran/Documents/M2_GENIOMHE/comparative_genomics/ParaAT_output/all_kaks.tsv",
+                   header = TRUE, sep = "\t", check.names = TRUE,
+                   comment.char = "")
+
+# See column names
+names(kaks)
+unique(kaks$Method)
+kaks_ma <- subset(kaks, Method == "MA")
+kaks_ma <- subset(kaks_ma,
+                  Ks > 0 & !is.na(Ka.Ks) & Ka.Ks >= 0 & Ka.Ks < 5)
+summary(kaks_ma$Ka.Ks)
+hist(kaks_ma$Ka.Ks,
+     breaks = 30,
+     main   = "Ka/Ks distribution – Danio rerio TE paralogs",
+     xlab   = "Ka/Ks",
+     ylab   = "Number of pairs")
+
+abline(v = 1, col = "red", lty = 2)  # neutral line
+plot(density(kaks_ma$Ka.Ks),
+     main = "Density of Ka/Ks ratios – Danio rerio TE paralogs",
+     xlab = "Ka/Ks")
+
+abline(v = 1, col = "red", lty = 2)
